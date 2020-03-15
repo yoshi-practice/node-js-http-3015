a31a96638cc9567e88d7ca81edbb38437d30053e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-03-15 23:45:42 +0900

    Update url-if

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
-'use strict';
-const http = require('http');
-const pug = require('pug');
-const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
+"use strict";
+const http = require("http");
+const pug = require("pug");
+const server = http
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info("[" + now + "] Requested by " + req.connection.remoteAddress);
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    });
 
-  switch (req.method) {
-    case 'GET':
-      // const fs = require('fs');
-      // const rs = fs.createReadStream('./form.html');
-      // rs.pipe(res);
-      res.write(pug.renderFile('./form.pug', {
-        path: req.url,
-        firstItem: '焼き肉',
-        secondItem: 'しゃぶしゃぶ'
-      }));
-      res.end();
-      break;
-    case 'POST':
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
+    switch (req.method) {
+      case "GET":
+        // const fs = require('fs');
+        // const rs = fs.createReadStream('./form.html');
+        // rs.pipe(res);
+        if (req.url === "/enquetes/yaki-shabu") {
+          res.write(
+            pug.renderFile("./form.pug", {
+              path: req.url,
+              firstItem: "焼き肉",
+              secondItem: "しゃぶしゃぶ"
+            })
+          );
+        } else if (req.url === "/enquetes/rice-bread") {
+        }
         res.end();
-      });
-      break;
-    default:
-      break;
-  }
-}).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
-}).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
-});
+        break;
+      case "POST":
+        let rawData = "";
+        req
+          .on("data", chunk => {
+            rawData = rawData + chunk;
+          })
+          .on("end", () => {
+            const decoded = decodeURIComponent(rawData);
+            console.info("[" + now + "] 投稿: " + decoded);
+            res.write(
+              '<!DOCTYPE html><html lang="ja"><body><h1>' +
+                decoded +
+                "が投稿されました</h1></body></html>"
+            );
+            res.end();
+          });
+        break;
+      default:
+        break;
+    }
+  })
+  .on("error", e => {
+    console.error("[" + new Date() + "] Server Error", e);
+  })
+  .on("clientError", e => {
+    console.error("[" + new Date() + "] Client Error", e);
+  });
 const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info("[" + new Date() + "] Listening on " + port);
 });
